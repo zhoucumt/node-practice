df338f9514d9f98a6847b9a7235bb8224dc835bf
zhoucumt <553817478@qq.com>
2020-08-15 15:23:56 +0800

    change

---
+++ src/mysql.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const cfg = {
+  host: 'localhost',
+  user: 'zqh',
+  password: 'zqh719719',
+  database: 'test'
+}
+
+module.exports = {
+  query: function (sql, value, callback) {
+    
+  }
+}
