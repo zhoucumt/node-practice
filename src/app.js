d43310742cea15045c23dfeb87b59cfb77c1f75f
zhoucumt <553817478@qq.com>
2020-08-16 08:20:20 +0800

    redux旧版实现compose

--- src/app.js
+++ src/app.js
@@ -9,23 +9,28 @@ app.use = function(fn) {
 };
 
 // app.compose.....
+// app.compose = function() {
+//   // 递归函数
+//   function dispatch(index) {
+//     // console.log('index: ', index);
+//     // 如果所有中间件都执行完跳出
+//     if (index === app.middlewares.length) return;
+
+//     // 取出第 index 个中间件并执行
+//     const route = app.middlewares[index];
+//     // console.log('route: ', route);
+//     // return route(() => dispatch(index + 1));
+//     // 执行后返回成功态的 Promise
+//     return Promise.resolve(route(() => dispatch(index + 1)));
+//   }
+
+//   // 取出第一个中间件函数执行
+//   dispatch(0);
+// };
+
+// redux旧版本实现
 app.compose = function() {
-  // 递归函数
-  function dispatch(index) {
-    // console.log('index: ', index);
-    // 如果所有中间件都执行完跳出
-    if (index === app.middlewares.length) return;
-
-    // 取出第 index 个中间件并执行
-    const route = app.middlewares[index];
-    // console.log('route: ', route);
-    // return route(() => dispatch(index + 1));
-    // 执行后返回成功态的 Promise
-    return Promise.resolve(route(() => dispatch(index + 1)));
-  }
-
-  // 取出第一个中间件函数执行
-  dispatch(0);
+  return app.middlewares.reduceRight((a, b) => () => b(a), () => {})();
 };
 
 module.exports = app;
