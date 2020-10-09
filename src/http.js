b2f6778784b2c025eb6d29b6760414e8acea7a3b
zhoucumt <553817478@qq.com>
2020-10-09 16:18:17 +0800

    change

--- src/http.js
+++ src/http.js
@@ -1,3 +1,6 @@
 const querystring = require("querystring");
+const url = require('url');
 const obj = querystring.parse('a=2&b=3');
-console.log(obj);
+const res = url.resolve('http://www.example.com/a/b/c', './one');
+// console.log(obj);
+console.log(res);
